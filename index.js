3ea90f47b0f9e39db4b7c1eac0ed6417386760c1
jukyg <safi2423f@gmail.com>
2026-02-26 15:35:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.listen(process.env.PORT || 3000);
 client.on('ready', async () => {
   console.log(`✅ SUCCESS: Logged in as ${client.user.tag}`);
   
-  // انتظر 3 ثواني قبل تعيين الـ presence
   setTimeout(() => {
     const r = new Discord.RichPresence()
       .setApplicationId('1476320950323642478')
@@ -18,13 +17,14 @@ client.on('ready', async () => {
       .setDetails('ME...')
       .setState('Watching ˢᵃᶤᶠ ₉₁')
       .setStartTimestamp(Date.now())
-      .setAssetsLargeImage('car') // استخدم اسم الصورة من Art Assets
+      // استخدم mp:attachments من Discord CDN
+      .setAssetsLargeImage('mp:attachments/1412519202560151614/1476344589416927394/car.jpg')
       .setAssetsLargeText('saif ₉₁')
       .addButton('ME ?', 'https://linktr.ee/povce')
       .addButton('server', 'https://discord.gg/3HzTN5rv');
     
     client.user.setActivity(r);
-    console.log('✅ Presence updated with asset: car');
+    console.log('✅ Presence updated with mp:attachments!');
   }, 3000);
 });
 
