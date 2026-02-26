b836857732628192c757a27ab25f50b35a1c3387
jukyg <safi2423f@gmail.com>
2026-02-26 15:30:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,23 @@ app.listen(process.env.PORT || 3000);
 client.on('ready', async () => {
   console.log(`✅ SUCCESS: Logged in as ${client.user.tag}`);
   
-  const r = new Discord.RichPresence()
-    .setApplicationId('1476320950323642478')
-    .setType('PLAYING')
-    .setName('saif ₉₁')
-    .setDetails('ME...')
-    .setState('Watching ˢᵃᶤᶠ ₉₁')
-    .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://i.imgur.com/8fEkSZC.jpeg')
-    .setAssetsLargeText('saif ₉₁')
-    .addButton('ME ?', 'https://linktr.ee/povce')
-    .addButton('server', 'https://discord.gg/3HzTN5rv');
-  
-  client.user.setActivity(r);
-  console.log('✅ Presence updated with Imgur image!');
+  // انتظر 3 ثواني قبل تعيين الـ presence
+  setTimeout(() => {
+    const r = new Discord.RichPresence()
+      .setApplicationId('1476320950323642478')
+      .setType('PLAYING')
+      .setName('saif ₉₁')
+      .setDetails('ME...')
+      .setState('Watching ˢᵃᶤᶠ ₉₁')
+      .setStartTimestamp(Date.now())
+      .setAssetsLargeImage('car') // استخدم اسم الصورة من Art Assets
+      .setAssetsLargeText('saif ₉₁')
+      .addButton('ME ?', 'https://linktr.ee/povce')
+      .addButton('server', 'https://discord.gg/3HzTN5rv');
+    
+    client.user.setActivity(r);
+    console.log('✅ Presence updated with asset: car');
+  }, 3000);
 });
 
 client.login(process.env.TOKEN);
